1f18eb71bf8185aa79f8e7e758a8276777530746
vyacheslavsafin <safin.vr@mail.ru>
2023-12-02 16:19:03 +0300

    Добавлены urls и контроллер в приложении users

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', include('habits.urls')),
-    #path('users/', include('users.urls')),
+    path('users/', include('users.urls', namespace='users')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
